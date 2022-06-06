c625ed483b04be6404d1bff9a67ed7a8e51ce873
TyNocks <54777351+TyNocks@users.noreply.github.com>
2022-06-06 15:24:28 -0400

    0.0.4d
    work on ingredient search

--- routes/ingredient.route.js
+++ routes/ingredient.route.js
@@ -1,58 +1,40 @@
 const express = require("express");
-const app = express();
 const ingredientRoutes = express.Router();
 const cors = require("cors");
-let Ingredient = require("../models/ingredient");
-let Nutrient = require("../models/nutrient");
+const config = require('../config')
 
-const allowedOrigins = [
-  "capacitor://localhost",
-  "ionic://localhost",
-  "http://localhost",
-  "http://localhost:8080",
-  "http://localhost:8100",
-];
 
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      //callback(new Error('Origin not allowed by CORS'));
-      callback(null, true);
-    }
-  },
-};
-ingredientRoutes.all("*", cors(corsOptions));
+ingredientRoutes.all("*", cors(config.corsOptions));
 
 ingredientRoutes.route("/search").post((req, res) => {
-  let regex = req.body.search.map((x) => new RegExp(x, "i"));
-  Ingredient.aggregate([
-    { $match: { $text: { $search: req.body.search.join(" ") } } },
-    {
-      $project: {
-        fdc_id: 1,
-        description: 1,
-        brand_owner: 1,
-        serving_size: 1,
-        serving_size_unit: 1,
-        score: { $meta: "textScore" },
-      },
-    },
-    { $sort: { description: 1 } },
-    { $sort: { score: { $meta: "textScore" } } },
-    { $match: { score: { $lt: 1.6 } } },
-    { $limit: 30 },
-  ])
-    .then((ingredients) => {
-      Ingredient.populate(
-        ingredients,
-        { path: "nutrients", match: { nutrient_id: 1008 } },
-        (err, results) => res.status(200).send(results)
-      );
-    })
-    .catch((error) => res.status(500).send(error));
+
+  foodDB = req.app.locals.db.db('Food').collection('Food');
+  nutrientDB = req.app.locals.db.db('Food').collection('Nutrient_Cleaned');
+
+  switch (req.body == undefined) {
+    case undefined:
+      res.status(205).send({ 'error': 'No meal data.' });
+      break;
+    default:
+      req.app.locals.db.db('Food').collection('Food')
+        .find({ '$text': { '$search': req.body.search } }, { 'score': { '$meta': 'textScore' } })
+        .sort({ 'score': { '$meta': 'textScore' } })
+        .limit(req.body.limit || 10)
+        .toArray()
+        .then(results => {
+          fdc_ids = results.map(x => x.fdc_id);
+          req.app.locals.db.db('Food').collection('Nutrient_Cleaned')
+            .find({ fdc_id: { '$in': fdc_ids } })
+            .then(nutrients => {
+              res.status(200).send({ foods: results, nutrients: nutrients });
+            });
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).send({ error: err });
+        });
+      break;
+  }
 });
 
 ingredientRoutes.route("/detailOne").post((req, res) => {
